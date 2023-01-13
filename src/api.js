ea9eb21de57b49044c29adf97076c6a4164f3655
Almog Levi <almoglevi71@gmail.com>
2023-01-13 21:29:22 +0200

    Uploading react weather app

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ffeab89165mshc6714210e6bcef2p19ecfejsn4bdcad0ad338',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "18a8aac6d3e405d65dbf1237e618ec55";
